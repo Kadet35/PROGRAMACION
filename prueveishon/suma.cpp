a9b4461141769d486cdc4574824dd5744ed42ea1
Kadet35 <kevin.bustos.reyes@utelvt.edu.ec>
2022-04-29 12:27:20 -0500

    ssiisis

---
+++ prueveishon/suma.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cout<<"ingrese valor 1: \n";
+	cin>>a;
+	cout<<"ingrese valor 2: \n";
+	cin>>b;
+	c=a+b;
+	cout<<" la suma es: \n"<<c;
+	return 0;
+}
